268eedd0245045d7def82a71c2eee4ce9d9899c8
Via Gonzales <145728325+ViaGonzales@users.noreply.github.com>
2023-12-03 21:00:03 +0800

    Add files via upload

---
+++ ProdCat-main/ProductCategory-main/ProductCategory-main/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::all();
+        return view('products.index', compact('products'));
+    }
+
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    public function store(Request $request)
+    {
+        // Validate incoming data
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'required|numeric',
+            'category_id' => 'required|exists:categories,id'
+        ]);
+
+        Product::create($validatedData);
+
+        return redirect()->route('products.index')->with('success', 'Product created successfully!');
+    }
+
+    // Implement other CRUD methods (edit, update, show, destroy)
+}
